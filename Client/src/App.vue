a36365b05c520e9a87746dd2e928ae69ab8b68fe
Christopher Okoro <65927932+christofa@users.noreply.github.com>
2022-07-04 19:31:24 -0700

    Add files via upload

---
+++ Client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <BlockGame/>
+  </div>
+</template>
+
+<script>
+  import BlockGame from './components/BlockGame.vue'
+
+export default {
+    name: 'app',
+      components: {  
+          BlockGame
+        }, 
+}
+</script> 
+
+<style>
+
+</style>
---
+++ server/index.js
@@ -0,0 +1,41 @@
+const Express = require("express")()
+const Http = require("http").Server(Express)
+const Socketio = require("socket.io")(Http, {
+        cors: {
+            origin: '*',
+            }
+});
+
+
+let position = {
+    x: 200,
+    y: 200
+}
+
+Socketio.on("connection", socket => {
+    socket.emit("position", position)
+    socket.on("move", data => {
+        switch(data) {
+            case "left":
+                position.x -= 5;
+                Socketio.emit("position", position)
+                break;
+            case "right":
+                position.x += 5;
+                Socketio.emit("position", position)
+                break;
+            case "up":
+                position.y -= 5;
+                Socketio.emit("position", position)
+                break;
+            case "down":
+                position.y += 5;
+                Socketio.emit("position", position)
+                break;
+        }
+    })
+})
+
+Http.listen(3000, () => {
+    console.log("Server up and running...")
+})
