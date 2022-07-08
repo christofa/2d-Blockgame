5f534efff29568d45b44a280a6d60977bf0cdeea
Christopher Okoro <65927932+christofa@users.noreply.github.com>
2022-07-08 13:42:02 -0700

    Update App.vue

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="app">
-      <BlockGame/>
+    <BlockGame />
   </div>
 </template>
 
 <script>
-  import BlockGame from './components/BlockGame.vue'
+import BlockGame from "./components/BlockGame.vue";
 
 export default {
-    name: 'app',
-      components: {  
-          BlockGame
-        }, 
-}
-</script> 
+  name: "app",
+  components: {
+    BlockGame,
+  },
+};
+</script>
 
-<style>
-
-</style>
+<style></style>
