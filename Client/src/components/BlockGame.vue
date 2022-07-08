24be17e8457c968b9633242c5eb08b869b01222a
Christopher Okoro <65927932+christofa@users.noreply.github.com>
2022-07-08 13:41:16 -0700

    Update BlockGame.vue

--- Client/src/components/BlockGame.vue
+++ Client/src/components/BlockGame.vue
@@ -1,55 +1,53 @@
 <template>
-<!-- creating the html canvas -->
-    <div>
-        <canvas 
-            ref="game" 
-            width="640" 
-            height="480" 
-            style="border: 1px solid black;">
-        </canvas>
+  <!-- creating the html canvas -->
+  <div>
+    <canvas ref="game" width="640" height="480" style="border: 1px solid black">
+    </canvas>
     <p>
-        <button v-on:click="move('right')">Right</button>
-        <button v-on:click="move('left')">Left </button>
-        <button v-on:click="move('up')">Up</button>
-        <button v-on:click="move('down')">Down</button>
+      <button v-on:click="move('right')">Right</button>
+      <button v-on:click="move('left')">Left</button>
+      <button v-on:click="move('up')">Up</button>
+      <button v-on:click="move('down')">Down</button>
     </p>
-    </div>
+  </div>
 </template>
 
 <script>
- import io from "socket.io-client"; //importing the socket.io we installed 
+import io from "socket.io-client"; //importing the socket.io we installed
 export default {
-    name: 'BlockGame',
-     data() {
-        return {
-            socket: {}, 
-            context: {},   
-            position: {
-                x: 0,
-                y: 0
-            }
-        }
-     },
-     created() {
-        this.socket = io("http://localhost:3000");
-     },
-     mounted() {
-        this.context = this.$refs.game.getContext("2d");
-        this.socket.on("position", data => {
-            this.position = data;
-            this.context.clearRect(0, 0, this.$refs.game.width, this.$refs.game.height)
-           this.context.fillRect(this.position.x, this.position.y, 20,20);
-        }); 
-         
-     },
-     methods: {
-        move(direction) {
-            this.socket.emit("move", direction)
-        }
-     }
-}
-</script> 
+  name: "BlockGame",
+  data() {
+    return {
+      socket: {},
+      context: {},
+      position: {
+        x: 0,
+        y: 0,
+      },
+    };
+  },
+  created() {
+    this.socket = io("http://localhost:3000");
+  },
+  mounted() {
+    this.context = this.$refs.game.getContext("2d");
+    this.socket.on("position", (data) => {
+      this.position = data;
+      this.context.clearRect(
+        0,
+        0,
+        this.$refs.game.width,
+        this.$refs.game.height
+      );
+      this.context.fillRect(this.position.x, this.position.y, 20, 20);
+    });
+  },
+  methods: {
+    move(direction) {
+      this.socket.emit("move", direction);
+    },
+  },
+};
+</script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
