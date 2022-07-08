f7cc3208ed10879ed80ea4a18e18c9626dbe7a05
Christopher Okoro <65927932+christofa@users.noreply.github.com>
2022-07-08 13:38:52 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,41 +1,40 @@
-const Express = require("express")()
-const Http = require("http").Server(Express)
+const Express = require("express")();
+const Http = require("http").Server(Express);
 const Socketio = require("socket.io")(Http, {
-        cors: {
-            origin: '*',
-            }
+  cors: {
+    origin: "*",
+  },
 });
 
-
 let position = {
-    x: 200,
-    y: 200
-}
+  x: 200,
+  y: 200,
+};
 
-Socketio.on("connection", socket => {
-    socket.emit("position", position)
-    socket.on("move", data => {
-        switch(data) {
-            case "left":
-                position.x -= 5;
-                Socketio.emit("position", position)
-                break;
-            case "right":
-                position.x += 5;
-                Socketio.emit("position", position)
-                break;
-            case "up":
-                position.y -= 5;
-                Socketio.emit("position", position)
-                break;
-            case "down":
-                position.y += 5;
-                Socketio.emit("position", position)
-                break;
-        }
-    })
-})
+Socketio.on("connection", (socket) => {
+  socket.emit("position", position);
+  socket.on("move", (data) => {
+    switch (data) {
+      case "left":
+        position.x -= 5;
+        Socketio.emit("position", position);
+        break; 
+      case "right":
+        position.x += 5;
+        Socketio.emit("position", position);
+        break;
+      case "up":
+        position.y -= 5;
+        Socketio.emit("position", position);
+        break;
+      case "down":
+        position.y += 5;
+        Socketio.emit("position", position);
+        break;
+    }
+  });
+});
 
 Http.listen(3000, () => {
-    console.log("Server up and running...")
-})
+  console.log("Server up and running...");
+});
